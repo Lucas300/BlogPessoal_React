32c20406646359cfc7750179738b4c7ee7a080ad
Lucas300 <ludaniel.sd@gmail.com>
2025-02-12 23:29:23 -0300

    Configurando rotas com React Router DOM e melhorando a estilização e responsiva com Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import Home from './pages/home/home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './components/navbar/Navbar'
+import Login from './pages/login/Login'
+import Home from './pages/home/Home'
+import Cadastro from './pages/cadastro/Cadastro'
+import Footer from './components/footer/Footer'
+
+
 
 function App() {
     return (
-        <Home />
+      <>
+          <BrowserRouter>
+            <Navbar />
+            <div className="min-h-[80vh]">
+              <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/cadastro" element={<Cadastro/>} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </div>
+            <Footer />
+          </BrowserRouter>
+      </>
     )
-}
-
-export default App
+  }
+  
+  export default App
